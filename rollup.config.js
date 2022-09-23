7a189a73865fb2a8bee88db115dd3324d042c7bf
Ben Farrell <ben@benfarrell.com>
2022-09-22 21:21:09 -0700

    add asset copying to tooling task

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import clean from 'rollup-plugin-clean';
 import html from '@web/rollup-plugin-html';
+import { copy } from '@web/rollup-plugin-copy';
 
 export default {
     input: 'index.html',
@@ -14,5 +15,6 @@ export default {
         html(),
         nodeResolve(),
         sourcemaps(),
+        copy({ patterns: 'assets/**' })
     ],
 };
